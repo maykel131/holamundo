27c2c01296829efc5c730e2bf9ba953499598fe3
maykel131 <trejomaykel@gmail.com>
2017-11-21 21:38:03 -0300

    corro
    hb

---
+++ enviar.php
@@ -0,0 +1,14 @@
+<?php 
+$destino="trejomaykel@gmail.com";
+$nombre=_POST['nombre'];
+$correo=_POST['correo'];
+$telefono=_POST['telefono'];
+$mensaje=_POST['mensaje'];
+
+$contenido ="nombre: " . $nombre . "\n Correo: " . $correo ."\n Telefono: " .$telefono. "\n Mensaje: " . $mensaje;
+
+mail($destino," Mensaje de la pagina ", $contenido);
+header("Location:gracias.html");
+
+
+ ?>
